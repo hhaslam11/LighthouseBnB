d0b2c1e3800d01bbf4a3153a7ea306b2794d3173
hhaslam11 <kalebmarc@hotmail.com>
2019-09-18 16:24:49 -0700

    add properties by city query

--- 1_queries/2_properties_by_city.sql
+++ 1_queries/2_properties_by_city.sql
@@ -1,6 +1,8 @@
 SELECT properties.*, AVG(property_reviews.rating) AS average_rating
 FROM properties
 JOIN property_reviews ON property_reviews.property_id = properties.id
--- WHERE AVG(property_reviews.rating) >= 4
+WHERE city LIKE '%ancouv%'
 GROUP BY properties.id
+HAVING AVG(property_reviews.rating) >= 4
+ORDER BY properties.cost_per_night
 LIMIT 10;
