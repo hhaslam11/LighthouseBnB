e7bd5ea793832a9f1981347ff332c2da5cf543d1
hhaslam11 <kalebmarc@hotmail.com>
2019-09-19 10:45:22 -0700

    fix return values and add addUser functionality

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -21,7 +21,7 @@ const getUserWithEmail = function(email) {
     SELECT * FROM users
     WHERE email = $1;
   `, [email])
-    .then(res => res.rows);
+    .then(res => res.rows[0]);
 };
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -35,7 +35,7 @@ const getUserWithId = function(id) {
     SELECT * FROM users
     WHERE id = $1;
   `, [id])
-    .then(res => res.rows);
+    .then(res => res.rows[0]);
 };
 exports.getUserWithId = getUserWithId;
 
@@ -46,10 +46,12 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool.query(`
+    INSERT INTO users (name, email, password)
+    VALUES ($1, $2, $3)
+    RETURNING *;
+  `, [user.name, user.email, user.password])
+    .then(res => res.rows[0]);
 };
 exports.addUser = addUser;
 
@@ -81,7 +83,6 @@ const getAllProperties = function(options, limit = 10) {
     .then(res => res.rows);
 };
 exports.getAllProperties = getAllProperties;
-
 
 /**
  * Add a property to the database
