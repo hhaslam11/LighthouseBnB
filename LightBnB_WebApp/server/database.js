6793b97abb4a7a25d4821ad24d477b7e36dd11ce
hhaslam11 <kalebmarc@hotmail.com>
2019-09-19 22:14:42 -0700

    remove unused console log

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -148,7 +148,6 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  console.log('WHAT IS IN THIS: ', property);
   return pool.query(`
     INSERT INTO properties (
                             title, description, number_of_bedrooms, number_of_bathrooms,
